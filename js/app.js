60c53b718211de79ac030457aa8539604d0eff87
linareyes <42820402+linareyes@users.noreply.github.com>
2018-09-21 18:26:55 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 $(document).foundation()
 
-/**this is for the index nav**/
-$(document).ready (function(){
-  $('.menu-toggle').click (function(){
-    $('nav').toggleClass('active')
-  })
-})
+
+
+
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
